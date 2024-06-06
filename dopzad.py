cf07d45ac16e007f3ab55801b18a4ddb94639ca3
Oleg <sagdeev.ol@yandex.ru>
2024-06-06 23:57:56 +0300

    Update dopzad.py

--- dopzad.py
+++ dopzad.py
@@ -1,35 +1,29 @@
 def calculate_structure_sum(data_structure):
-     x = 0 # здесь будем собирать данные типа int
-     y = 0 # здесь будем собирать данные типа str
-     s = x + y  # здесь будем собирать данные типа str и int
-     for i in data_structure:
-         if type(i) == str:
-            y += len(i)
-            s = x + y # здесь будем собирать данные типа str и int
-         if type(i) == int:
-            x += i
-     for j in data_structure(i):
-         if type(j) == list or type(j) == tuple:
-            if type(i) == str:
-                    y += len(i)
-            if type(i) == int:
-                    x += i
-            if type(j) == dict:
-                if type(i) == str:
-                    y += len(i)
-                if type(i) == int:
-                    x += i
-     return s
+    symma = 0
+    if isinstance(data_structure, int):  # проверяем содержит ли параметр функции тип данных int
+        return data_structure
+    if isinstance(data_structure, str):  # проверяем содержит ли параметр функции тип данных str
+        return len(data_structure)
+    if isinstance(data_structure, (list, tuple, set)):  # проверяем содержит ли параметр функции тип данных list, tuple, set
+        for i in data_structure:
+            symma += calculate_structure_sum(i)  # добавляем i-й элемент к сумме при условиях описанных в строке 3 и 5, если не подходит, функция переходит в строку 9 или возвращается в строку 7
+    if isinstance(data_structure, dict):  # проверяем содержит ли параметр функции тип данных dict
+        for key, value in data_structure.items():
+            symma += calculate_structure_sum(key)  # добавляем значения ключа к сумме при условиях описанных в строке 3 и 5
+            symma += calculate_structure_sum(value)  # добавляем значения величины к сумме при условиях описанных в строке 3 и 5
+    return summa
 
 
 
-result = calculate_structure_sum([
+
+data_structure=[
     [1, 2, 3],
     {'a': 4, 'b': 5},
     (6, {'cube': 7, 'drum': 8}),
     "Hello",
     ((), [{(2, 'Urban', ('Urban2', 35))}])
-])
+]
+result = calculate_structure_sum(data_structure)
 print(result)
 
 
